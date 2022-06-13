f12ed3e68ee526966d8751efec074e051abbf4bc
William <williamalvarez672@gmail.com>
2022-06-13 13:08:44 +0100

    powershell profile

---
+++ powershell/userprofile.ps1
@@ -0,0 +1,26 @@
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/williamalvarez92/personal-config/main/oh-my-posh/neko.omp.json'| Invoke-Expression
+
+#Alias
+
+Set-Alias vim nvim
+Set-Alias // clear
+
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}

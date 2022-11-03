1eddf3eb483921ae337eec0307eb8a10a78decca
William Alvarez <williamalvarez672@gmail.com>
2022-11-03 11:01:24 +0000

    Update userprofile.ps1

--- powershell/userprofile.ps1
+++ powershell/userprofile.ps1
@@ -11,6 +11,7 @@ Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Fzf
 Import-Module PSFzf

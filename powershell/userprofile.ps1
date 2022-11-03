e2303866bc727c0594e737116d5bf6f7d7b73b8e
William Alvarez <williamalvarez672@gmail.com>
2022-11-03 11:05:46 +0000

    Update userprofile.ps1

--- powershell/userprofile.ps1
+++ powershell/userprofile.ps1
@@ -9,7 +9,6 @@ Set-Alias // clear
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 

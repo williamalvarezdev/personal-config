84ba811609b9a7b004870ae85dd45441c1a21b32
William Alvarez <williamalvarez672@gmail.com>
2024-05-11 14:20:43 +0100

    changin all of it

---
+++ Windows/setup.ps1
@@ -0,0 +1,26 @@
+# Check if Oh My Posh is already installed
+if (-not (Get-Command oh-my-posh -ErrorAction SilentlyContinue)) {
+    # Install Oh My Posh using winget
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+    Write-Host "Oh My Posh installed successfully."
+    # URL of the JSON file
+    $url = "http://www.us.com/config.json"
+
+    # Get current user's username
+    $user = $env:USERNAME
+
+    # Path to save the JSON file locally
+    $localPath = "C:\Users\$user\AppData\Local\oh-my-posh\config.json"
+
+    # Download the JSON file
+    Invoke-WebRequest -Uri $url -OutFile $localPath
+
+    # Check if the file has been downloaded successfully
+    if (Test-Path $localPath) {
+        Write-Host "JSON file downloaded and saved successfully."
+    } else {
+        Write-Host "Failed to download JSON file."
+    }
+} else {
+    Write-Host "Oh My Posh is already installed."
+}
--- powershell/README.md
+++
@@ -1,2 +0,0 @@
-Got inspiration from
-https://www.youtube.com/watch?v=5-aK2_WwrmM&t=2130s
